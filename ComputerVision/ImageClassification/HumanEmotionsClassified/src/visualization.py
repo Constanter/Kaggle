908853670a760cb52cfc6da1e298a8b04efe3c0b
tekhin <tekhin@cvisionlab.com>
2023-09-09 13:45:21 +0300

    chore: add training process visualization

--- ComputerVision/ImageClassification/HumanEmotionsClassified/src/visualization.py
+++ ComputerVision/ImageClassification/HumanEmotionsClassified/src/visualization.py
@@ -11,3 +11,30 @@ def visualize(dataset, names):
             plt.title(names[tf.argmax(lbl[i], axis=0).numpy()])
             plt.axis('off')
         plt.show()
+
+
+def get_visualizitons_training(history, save_path,need_to_show=False, loss=True):
+    if loss:
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.title('model loss')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'val_loss'])
+        if need_to_show:
+            plt.show()
+        else:
+            plt.savefig(f'{save_path}/loss.png')
+        plt.close()
+    else:
+        plt.plot(history.history['accuracy'])
+        plt.plot(history.history['val_accuracy'])
+        plt.title('model performance')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'val'])
+        if need_to_show:
+            plt.show()
+        else:
+            plt.savefig(f'{save_path}/performance.png')
+        plt.close()
