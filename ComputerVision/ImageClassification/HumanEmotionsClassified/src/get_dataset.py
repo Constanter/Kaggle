aca9ff15b8550e59744cfda22dceb619badfc5a2
tekhin <tekhin@cvisionlab.com>
2023-09-09 13:46:02 +0300

    chore: refactor hyperparameters

--- ComputerVision/ImageClassification/HumanEmotionsClassified/src/get_dataset.py
+++ ComputerVision/ImageClassification/HumanEmotionsClassified/src/get_dataset.py
@@ -1,12 +1,7 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Resizing, Rescaling
-
-CLASS_NAMES = ('angry', 'happy', 'sad')
-CONFIGURATION = {'BATCH_SIZE': 16,
-                 'IM_SIZE': 224}
 
 
-def get_datasets(data_folder):
+def get_datasets(data_folder, batch_size, im_size, class_names):
     train_directory = f'{data_folder}/train'
     test_directory = f'{data_folder}/test'
 
@@ -14,10 +9,10 @@ def get_datasets(data_folder):
         train_directory,
         labels='inferred',
         label_mode='categorical',
-        class_names=CLASS_NAMES,
+        class_names=class_names,
         color_mode='rgb',
-        batch_size=CONFIGURATION['BATCH_SIZE'],
-        image_size=(CONFIGURATION['IM_SIZE'], CONFIGURATION['IM_SIZE']),
+        batch_size=im_size,
+        image_size=(im_size, im_size),
         shuffle=True,
         seed=13
     )
@@ -26,11 +21,11 @@ def get_datasets(data_folder):
         test_directory,
         labels='inferred',
         label_mode='categorical',
-        class_names=CLASS_NAMES,
+        class_names=class_names,
         color_mode='rgb',
-        batch_size=CONFIGURATION['BATCH_SIZE'],
-        image_size=(CONFIGURATION['IM_SIZE'], CONFIGURATION['IM_SIZE']),
-        shuffle=True,
+        batch_size=batch_size,
+        image_size=(im_size, im_size),
+        shuffle=False,
         seed=13
     )
 
